4ba154787dade57104b5732687e8b0dcc26178c8
bklimek123 <brianklimek3@gmail.com>
2024-06-20 21:06:46 -0500

    j

--- js/index.js
+++ js/index.js
@@ -4,26 +4,27 @@ $(document).ready(function() {
     // Event listener for card buttons
     $(".card-button").click(function() {
         console.log("Card button was clicked!");
-// Additional UI interaction: Change button color to indicate it was clicked
+        // Additional UI interaction: Change button color to indicate it was clicked
         $(this).css("background-color", "#2B0150");
     });
+
+    // Event listener for card buttons hover
+    $(".card-button").hover(
+        function() {
+            // Mouse enters the element
+            $(this).css({
+                "background-color": "#FFFFFF",
+                "border-color": "#FFFFFF"
+            });
+            $(this).css("color", "#5A00A7"); // Change the arrow color to #5A00A7
+        }, 
+        function() {
+            // Mouse leaves the element
+            $(this).css({
+                "background-color": "#5A00A7",
+                "border-color": "#5A00A7"
+            });
+            $(this).css("color", "#FFFFFF"); // Revert the arrow color to #FFFFFF
+        }
+    );
 });
- // Event listener for card buttons hover
- $(".card-button").hover(
-    function() {
-        // Mouse enters the element
-        $(this).css({
-            "background-color": "#FFFFFF",
-            "border-color": "#FFFFFF"
-        });
-        $(this).css("color", "#5A00A7"); // Change the arrow color to #5A00A7
-    }, 
-    function() {
-        // Mouse leaves the element
-        $(this).css({
-            "background-color": "#5A00A7",
-            "border-color": "#5A00A7"
-        });
-        $(this).css("color", "#FFFFFF"); // Revert the arrow color to #FFFFFF
-    }
-);
